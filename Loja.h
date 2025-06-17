2b41769722697e15867060ceba69e036124cb08e
Dev_Dorado <fdpvieira@gmail.com>
2025-06-17 18:40:34 +0100

    Create Loja.h

---
+++ Loja.h
@@ -0,0 +1,54 @@
+#pragma once
+// Loja.h
+//#ifndef LOJA_H
+//#define LOJA_H
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <ctime>
+#include "produtos.h"
+#include "cliente.h"
+#include "venda.h"
+
+using namespace std;
+
+class Loja {
+private:
+    vector<Produto> produtos;
+    vector<Cliente> clientes;
+    vector<Venda> vendas;
+
+    int proximoIdProduto;
+    int proximoIdCliente;
+    int proximoNumeroFatura;
+
+public:
+    Loja();
+
+    // Produtos
+    void criarProduto();
+    void eliminarProduto();
+    void criarProduto(const string& nome, int quantidade, float precoCusto);
+    void adicionarStock(int idProduto, int quantidade);
+    void eliminarProduto(int idProduto);
+    void listarProdutos() const;
+    void relatorioGraficoVendas() const;
+
+
+    // Clientes
+    void criarCliente(const string& nome, const string& telefone, const string& morada);
+    void eliminarCliente(int idCliente);
+    void alterarNomeCliente(int idCliente, const string& novoNome);
+    void listarClientes() const;
+
+    // Vendas
+    void efetuarVenda(int idCliente);
+
+    // Relatórios
+    void relatorioStock() const;
+    void relatorioVendasPorProduto(const string& nomeProduto) const;
+    void relatorioTotalVendas() const;
+};
+
+//#endif
